48b489b63d48d713c0bce9d5dc5b48e0c26d788a
zhj149 <zhj149@163.com>
2018-01-31 08:28:01 +0800

    比例尺算法不对

--- geotools-swing/src/main/java/org/geotools/geotools_swing/plugins/ScalebarPlugin.java
+++ geotools-swing/src/main/java/org/geotools/geotools_swing/plugins/ScalebarPlugin.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 
 import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.geometry.jts.ReferencedEnvelope;
@@ -48,34 +49,84 @@ public class ScalebarPlugin extends PluginBase {
 	 * 计算地图横跨范围结束点的坐标
 	 */
 	private Point pointEnd;
+	
+	/**
+	 * 标尺最大长度，默认5cm
+	 */
+	private double maxLength = 0.05;
+	
+	/**
+	 * 默认的标尺宽度像素数
+	 */
+	private int maxPixels;
+	
+	/**
+	 * 标尺最大长度，默认2cm
+	 */
+	private double minLength = 0.02;
+	
+	/**
+	 * 标尺的最小像素数
+	 */
+	private int minPixels;
+	
+	/**
+	 * 当前显示的dpi
+	 */
+	private int dpi = 96;
 
 	/**
 	 * 初始化默认标尺表示范围
 	 */
 	static {
+//		rulers.put(0.0, new Pair<Integer, String>(1, "1m"));
+//		rulers.put(1.01, new Pair<Integer, String>(1, "2m"));
+//		rulers.put(2.01, new Pair<Integer, String>(2, "5m"));
+//		rulers.put(5.01, new Pair<Integer, String>(5, "10m"));
+//		rulers.put(10.01, new Pair<Integer, String>(10, "20m"));
+//		rulers.put(20.01, new Pair<Integer, String>(20, "50m"));
+//		rulers.put(50.01, new Pair<Integer, String>(50, "100m"));
+//		rulers.put(100.01, new Pair<Integer, String>(100, "200m"));
+//		rulers.put(200.01, new Pair<Integer, String>(200, "500m"));
+//		rulers.put(500.01, new Pair<Integer, String>(500, "1km"));
+//		rulers.put(1000.01, new Pair<Integer, String>(1000, "2km"));
+//		rulers.put(2000.01, new Pair<Integer, String>(2000, "5km"));
+//		rulers.put(5000.01, new Pair<Integer, String>(5000, "10km"));
+//		rulers.put(10000.01, new Pair<Integer, String>(10000, "20km"));
+//		rulers.put(20000.01, new Pair<Integer, String>(20000, "50km"));
+//		rulers.put(50000.01, new Pair<Integer, String>(50000, "100km"));
+//		rulers.put(100000.01, new Pair<Integer, String>(100000, "200km"));
+//		rulers.put(200000.01, new Pair<Integer, String>(200000, "500km"));
+//		rulers.put(500000.01, new Pair<Integer, String>(500000, "1000km"));
+//		rulers.put(1000000.01, new Pair<Integer, String>(1000000, "2000km"));
+//		rulers.put(2000000.01, new Pair<Integer, String>(2000000, "5000km"));
+//		rulers.put(5000000.01, new Pair<Integer, String>(5000000, "10000km"));
+//		rulers.put(10000000.01, new Pair<Integer, String>(10000000, "20000km"));
+		
 		rulers.put(0.0, new Pair<Integer, String>(1, "1m"));
-		rulers.put(1.01, new Pair<Integer, String>(2, "2m"));
-		rulers.put(2.01, new Pair<Integer, String>(5, "5m"));
-		rulers.put(5.01, new Pair<Integer, String>(10, "10m"));
-		rulers.put(10.01, new Pair<Integer, String>(20, "20m"));
-		rulers.put(20.01, new Pair<Integer, String>(50, "50m"));
-		rulers.put(50.01, new Pair<Integer, String>(100, "100m"));
-		rulers.put(100.01, new Pair<Integer, String>(200, "200m"));
-		rulers.put(200.01, new Pair<Integer, String>(500, "500m"));
-		rulers.put(500.01, new Pair<Integer, String>(1000, "1km"));
-		rulers.put(1000.01, new Pair<Integer, String>(2000, "2km"));
-		rulers.put(2000.01, new Pair<Integer, String>(5000, "5km"));
-		rulers.put(5000.01, new Pair<Integer, String>(10000, "10km"));
-		rulers.put(10000.01, new Pair<Integer, String>(20000, "20km"));
-		rulers.put(20000.01, new Pair<Integer, String>(50000, "50km"));
-		rulers.put(50000.01, new Pair<Integer, String>(100000, "100km"));
-		rulers.put(100000.01, new Pair<Integer, String>(200000, "200km"));
-		rulers.put(200000.01, new Pair<Integer, String>(500000, "500km"));
-		rulers.put(500000.01, new Pair<Integer, String>(1000000, "1000km"));
-		rulers.put(1000000.01, new Pair<Integer, String>(2000000, "2000km"));
-		rulers.put(2000000.01, new Pair<Integer, String>(5000000, "5000km"));
-		rulers.put(5000000.01, new Pair<Integer, String>(10000000, "10000km"));
-		rulers.put(10000000.01, new Pair<Integer, String>(20000000, "20000km"));
+		rulers.put(0.5, new Pair<Integer, String>(1, "2m"));
+		rulers.put(1.5, new Pair<Integer, String>(2, "5m"));
+		rulers.put(7.5, new Pair<Integer, String>(5, "10m"));
+		rulers.put(15.0, new Pair<Integer, String>(10, "20m"));
+		rulers.put(25.0, new Pair<Integer, String>(20, "50m"));
+		rulers.put(75.0, new Pair<Integer, String>(50, "100m"));
+		rulers.put(150.0, new Pair<Integer, String>(100, "200m"));
+		rulers.put(250.0, new Pair<Integer, String>(200, "500m"));
+		rulers.put(750.0, new Pair<Integer, String>(500, "1km"));
+		rulers.put(1500.0, new Pair<Integer, String>(1000, "2km"));
+		rulers.put(2500.0, new Pair<Integer, String>(2000, "5km"));
+		rulers.put(7500.0, new Pair<Integer, String>(5000, "10km"));
+		rulers.put(15000.0, new Pair<Integer, String>(10000, "20km"));
+		rulers.put(25000.0, new Pair<Integer, String>(20000, "50km"));
+		rulers.put(75000.0, new Pair<Integer, String>(50000, "100km"));
+		rulers.put(150000.0, new Pair<Integer, String>(100000, "200km"));
+		rulers.put(250000.0, new Pair<Integer, String>(200000, "500km"));
+		rulers.put(750000.0, new Pair<Integer, String>(500000, "1000km"));
+		rulers.put(1500000.0, new Pair<Integer, String>(1000000, "2000km"));
+		rulers.put(2500000.0, new Pair<Integer, String>(2000000, "5000km"));
+		rulers.put(7500000.0, new Pair<Integer, String>(5000000, "10000km"));
+		rulers.put(15000000.0, new Pair<Integer, String>(10000000, "20000km"));
+		
 	}
 
 	/**
@@ -89,6 +140,8 @@ public class ScalebarPlugin extends PluginBase {
 		super(title, group, map);
 		pointBegin = gf.createPoint(new Coordinate());
 		pointEnd = gf.createPoint(new Coordinate());
+		dpi = Toolkit.getDefaultToolkit().getScreenResolution();
+		
 	}
 
 	/**
@@ -112,7 +165,7 @@ public class ScalebarPlugin extends PluginBase {
 		Rectangle location = mapPane.getVisibleRectangle();
 		Pair<Integer, String> ruler = rulers.get(distance);
 		int pwidth = ruler.getKey();
-		double x = pwidth * location.getWidth() / distance; //真正的标尺代表的宽度
+		double x = pwidth * location.getWidth() / distance / 64; //真正的标尺代表的宽度
 		if (x < 1)
 			x = 1;
 
